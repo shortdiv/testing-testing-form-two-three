d480f803d50e2a0c35ddaa6633a142d08605172a
Divya Sasidharan <hello@shortdiv.com>
2019-03-14 19:01:17 -0500

    enable password lock

--- src/lambda/lock-site.js
+++ src/lambda/lock-site.js
@@ -1,7 +1,24 @@
+import axios from "axios";
+
 export function handler(event, context, callback) {
   console.log("hi");
   console.log(event);
   console.log(context);
+
+  const headers = event.headers;
+
+  axios({
+    url: `https://api.netlify.com/api/v1/sites/${
+      event.headers["x-bb-client-request-uuid"]
+    }/`,
+    headers: { Authorization: `Bearer ${headers}` },
+    method: "PUT",
+    data: {
+      password: true
+    }
+  }).then(res => {
+    console.log(res);
+  });
 
   callback(null, {
     statusCode: 200,
